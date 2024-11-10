e0661707eef050131fc9e7d8f7df323dc59e1245
kochamPeaxa <pojska@op.pl>
2024-11-10 22:13:25 +0100

    trzecia migawka-sprawdzenie

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main(){
-	int x,y;
+	int x,y;	
 	std::cin>>x>>y;
+	if(x<=0 || y<=0){
+		std::cout<<"wartosc niedodatnia";
+		return 0;
+	}
 	std::cout<<x<<y;
 	return 0;
 }
