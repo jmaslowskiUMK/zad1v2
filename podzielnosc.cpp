047dbe78f3a975f966ea6af09381df764b1dff0e
kochamPeaxa <pojska@op.pl>
2024-11-10 22:07:13 +0100

    druga migawka-nwd

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+int nwd(int a,int b){
+	while(a!=b){
+		if(a>b)a-=b;
+		else b-=a;
+	}
+	return a;
+}
+
 int main(){
 	int x,y;
 	std::cin>>x>>y;
-	std::cout<<x<<y;
+	std::cout<<x<<" "<<y<<" "<<nwd(x,y);
 	return 0;
 }
